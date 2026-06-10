9b5fc36552dbe6cf3ba1feee20a6525c0c2bb5dd
Taranpreet Singh <taranpreet.bhamara@gmail.com>
2026-06-10 09:32:02 +0530

    Time: 0 ms (100%), Space: 20 MB (10.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,23 +2,37 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> freq;
-        for (int i : nums) {
-            freq[i]++;
+        vector<vector<int>> vec(nums.size() + 1);
+        for (int num : nums) {
+            freq[num]++;
         } // O(n)
 
-        vector<pair<int, int>> arr;
-
-        for (auto& [key, val] : freq) {
-            arr.push_back({val, key});
+        for (const auto& entry : freq) {
+            vec[entry.second].push_back(entry.first);
         }
-
-        sort(arr.rbegin(), arr.rend());
 
         vector<int> res;
-        for (int i = 0; i < k; i++) {
-            res.push_back(arr[i].second);
+        for (int i = vec.size() - 1; i > 0; i--) {
+            for (int n : vec[i]) {
+                res.push_back(n);
+                if (res.size() == k) {
+                    return res;
+                }
+            }
         }
 
-        return vector<int>(res.begin(), res.begin() + k);
+        return res;
+        // for (auto& [key, val] : freq) {
+        //     arr.push_back({val, key});
+        // } // O(n)
+
+        // sort(arr.rbegin(), arr.rend()); // O(nlogn)
+
+        // vector<int> res;
+        // for (int i = 0; i < k; i++) {
+        //     res.push_back(arr[i].second);
+        // }
+
+        // return vector<int>(res.begin(), res.begin() + k);
     }
 };
