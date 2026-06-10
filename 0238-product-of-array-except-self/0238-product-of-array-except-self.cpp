769a85dd698b72add02b27205e1d7065434cdaf9
Taranpreet Singh <taranpreet.bhamara@gmail.com>
2026-06-10 10:10:17 +0530

    Time: 3 ms (37.8%), Space: 40.1 MB (95.3%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int> divisionMethod(vector<int>& nums) {
         vector<int> res(nums.size(), 0);
         int prod = 1, zeroCount = 0;
         for (int num : nums) {
@@ -24,5 +24,26 @@ public:
         }
 
         return res;
+    } // T: O(n) S: O(1)
+
+    vector<int> prefixSuffixMethod(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 1);
+
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        int postfix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+
+    } // T: O(n) S: O(1)
+
+    vector<int> productExceptSelf(vector<int>& nums) {
+        return prefixSuffixMethod(nums);
     }
 };
