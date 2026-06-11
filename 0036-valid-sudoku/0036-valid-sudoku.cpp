085e81f8219c090020d8ec98b5b602ee733fb718
Taranpreet Singh <taranpreet.bhamara@gmail.com>
2026-06-11 18:44:29 +0530

    Time: 0 ms (100%), Space: 24.1 MB (39.42%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -4,31 +4,34 @@ public:
         for (int i = 0; i < 9; i++) {
             unordered_map<char, int> row;
             unordered_map<char, int> col;
+            unordered_set<char> seen;
+
+            // for rows and cols
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') {
-                    if (row.count(board[i][j])){
+                    if (row.count(board[i][j])) {
                         return false;
                     }
                     row[board[i][j]]++;
                 }
 
-                if (board[j][i] != '.'){
-                    if(col.count(board[j][i])){
+                if (board[j][i] != '.') {
+                    if (col.count(board[j][i])) {
                         return false;
                     }
                     col[board[j][i]]++;
                 }
             }
-        }
 
-        for (int square = 0; square < 9; square++) {
-            unordered_set<char> seen;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    int row = (square / 3) * 3 + i;
-                    int col = (square % 3) * 3 + j;
-                    if (board[row][col] == '.') continue;
-                    if (seen.count(board[row][col])) return false;
+            // For square
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    int row = (i / 3) * 3 + j;
+                    int col = (i % 3) * 3 + k;
+                    if (board[row][col] == '.')
+                        continue;
+                    if (seen.count(board[row][col]))
+                        return false;
                     seen.insert(board[row][col]);
                 }
             }
