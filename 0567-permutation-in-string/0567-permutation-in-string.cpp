5cbcafd9c7d03d2815e4bbed51608eaf175122d7
Taranpreet Singh <taranpreet.bhamara@gmail.com>
2026-06-28 17:59:04 +0530

    Time: 1043 ms (5.38%), Space: 13.1 MB (13.71%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char, int> mp;
+        for (auto c : s1) {
+            mp[c]++;
+        }
+
+        bool reset = true;
+        unordered_map<char, int> mp_copy;
+        int l = 0, r = 0;
+        while (r < s2.size()) {
+            char curr = s2[r];
+            if (reset) {
+                mp_copy = mp;
+                reset = false;
+            }
+            if (mp_copy.count(curr)) {
+                r++;
+                mp_copy[curr]--;
+                if (mp_copy[curr] == 0)
+                    mp_copy.erase(curr);
+
+                if (mp_copy.size() == 0)
+                    return true;
+            } else {
+                l++;
+                r = l;
+                reset = true;
+            }
+        }
+
+        return false;
+    }
+};
