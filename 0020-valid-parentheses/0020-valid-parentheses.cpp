417533c66e7043f5081dd406c114d937c6031cc2
Taranpreet Singh <taranpreet.bhamara@gmail.com>
2026-07-07 10:01:36 +0530

    Time: 0 ms (100%), Space: 9.1 MB (18.55%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        unordered_map<char, char> closeToOpen = {
+            {')', '('}, {']', '['}, {'}', '{'}};
+
+        for (auto c : s) {
+            if (closeToOpen.count(c)) {
+                if (!stack.empty() && stack.top() == closeToOpen[c]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
+};
